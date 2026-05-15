af65b4d229db50cac870e1715a247dccb10c076c
FloatingMilkshake <me@floatingmilkshake.com>
2026-05-14 23:32:05 -0400

    wikihow: Use different method to fetch images
    The WikiHow API seems to be broken and this works for now

--- commands/fun/wikihow.js
+++ commands/fun/wikihow.js
@@ -9,20 +9,29 @@ class WikihowCommand extends Command {
       controller.abort();
     }, 15000);
     try {
-      const req = await fetch("https://www.wikihow.com/api.php?action=query&generator=random&prop=imageinfo&format=json&iiprop=url&grnnamespace=6", { signal: controller.signal });
+      //const req = await fetch("https://www.wikihow.com/api.php?action=query&generator=random&prop=imageinfo&format=json&iiprop=url&grnnamespace=6", { signal: controller.signal });
+      //clearTimeout(timeout);
+      //const json = await req.json();
+      //const id = Object.keys(json.query.pages)[0];
+      //const data = json.query.pages[id];
+      //if (data.imageinfo) {
+      //  return json.query.pages[id].imageinfo[0].url;
+      //} else {
+      //  return await this.run();
+      //}
+
+      const req = await fetch("https://www.wikihow.com/Special:Randomizer", { signal: controller.signal });
       clearTimeout(timeout);
-      const json = await req.json();
-      const id = Object.keys(json.query.pages)[0];
-      const data = json.query.pages[id];
-      if (data.imageinfo) {
-        return json.query.pages[id].imageinfo[0].url;
-      } else {
-        return await this.run();
-      }
+      const regexp = /(?:https:\/\/www\.wikihow\.com)?\/images\/thumb\/([0-9a-f]\/[0-9a-f]{2}\/[^"' <]+?\.(?:jpg|jpeg|png|gif))(?:\/[^"' <]+)?(?:\s+\d+w)?/gi;
+      const matches = Array.from((await req.text()).matchAll(regexp));
+      return "https://www.wikihow.com/images/" + matches[Math.floor(Math.random() * matches.length)][1];
     } catch (e) {
       if (e.name === "AbortError") {
         this.success = false;
         return "I couldn't get a WikiHow image in time. Maybe try again?";
+      } else if (e.name === "SyntaxError") {
+        this.success = false;
+        return "I couldn't get a WikiHow image. Something is probably wrong with their API. Maybe try again later?"
       }
     }
   }
